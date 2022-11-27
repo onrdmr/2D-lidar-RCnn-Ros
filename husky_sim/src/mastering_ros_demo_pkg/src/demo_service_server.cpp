45e61e64b25f6691d025831cb639657ea6d95406
onrdmr <onur.demir2@std.yildiz.edu.tr>
2022-11-27 21:11:43 +0300

    qt kodu eklendi üzerine gerekli bileşenler yazılacak.

--- husky_sim/src/mastering_ros_demo_pkg/src/demo_service_server.cpp
+++ husky_sim/src/mastering_ros_demo_pkg/src/demo_service_server.cpp
@@ -40,82 +40,93 @@
 
 using namespace std;
 
-struct Message
+class MessageHandler
 {
-  int wallSequence;
-  int robotPositionSequence;
-};
-
-bool demo_service_callback(mastering_ros_demo_pkg::demo_srv::Request& req,
-                           mastering_ros_demo_pkg::demo_srv::Response& res)
-{
-  std::stringstream ss;
-  ss << "walls1";
-  res.out = ss.str();
-  // write here to exploration number and remaining exploration number
-  // if remaining exloration number decrease, run map creation exec
-  // batch init is send by server to factory.cc
-  // exploration_scan before starting read from this server information abot which map is used.
-  ROS_INFO("From Client [%s], Server says [%s]", req.in.c_str(), res.out.c_str());
-
-  // double simTime = ros::Time::now().toSec();
-  // if (simTime > 20)
-  // {
-  //   ros::NodeHandle n;
-  //   ros::ServiceClient resetGazebo = n.serviceClient<std_srvs::Empty>("/gazebo/reset_world");
-  //   std_srvs::Empty srv;
-
-  //   if (resetGazebo.call(srv))
-  //   {
-  //     ROS_INFO("Resettig Gazebo World");
-  //   }
-  // }
-  return true;
-}
-
-void number_callback(const rosgraph_msgs::Clock::ConstPtr& msg)
-{
-  // ROS_INFO_STREAM("Received " << msg->clock << ros::Time::now());
-
-  std::stringstream str;
-  str << msg->clock;
-  double simTime = std::stod(str.str());
-  if (simTime > 10)
+public:
+  static void thread_demo_service_server(int argc, char** argv, ros::MultiThreadedSpinner spinner)
   {
+    ros::init(argc, argv, "demo_service_server");
     ros::NodeHandle n;
-    ros::ServiceClient resetGazebo = n.serviceClient<std_srvs::Empty>("/gazebo/reset_simulation");
-    std_srvs::Empty srv;
+    ros::ServiceServer service = n.advertiseService("demo_service", demo_service_callback);
+    spinner.spin();
+  }
 
-    if (resetGazebo.call(srv))
+  static void thread_demo_clock_subscriber(int argc, char** argv, ros::MultiThreadedSpinner spinner)
+  {
+    ros::init(argc, argv, "clock_subscriber");
+
+    ros::NodeHandle node_obj;
+    std::cout << "deneme " << std::endl;
+    ros::Subscriber number_subscriber = node_obj.subscribe("/clock", 1, number_callback);
+    spinner.spin();
+  }
+
+private:
+  static void number_callback(const rosgraph_msgs::Clock::ConstPtr& msg)
+  {
+    // ROS_INFO_STREAM("Received " << msg->clock << ros::Time::now());
+
+    std::stringstream str;
+    str << msg->clock;
+    double simTime = std::stod(str.str());
+    if (simTime > 10)
     {
-      ROS_INFO_STREAM("Received " << msg->clock << ros::Time::now());
-      ROS_INFO("Resettig Gazebo World");
-      ros::Duration(1).sleep();
+      ros::NodeHandle n;
+      ros::ServiceClient resetGazebo = n.serviceClient<std_srvs::Empty>("/gazebo/reset_simulation");
+      std_srvs::Empty srv;
+
+      if (resetGazebo.call(srv))
+      {
+        ROS_INFO_STREAM("Received " << msg->clock << ros::Time::now());
+        ROS_INFO("Resettig Gazebo World");
+        ros::Duration(1).sleep();
+        // assign new positon or new map if all position are loaded
+        // print qui way of how much map is printed in memory
+      }
     }
   }
-}
+  static bool demo_service_callback(mastering_ros_demo_pkg::demo_srv::Request& req,
+                                    mastering_ros_demo_pkg::demo_srv::Response& res)
+  {
+    std::stringstream ss;
+    ss << "walls1";
+    res.out = ss.str();
 
-void thread_demo_service_server(int argc, char** argv)
-{
-  ros::init(argc, argv, "demo_service_server");
-  ros::NodeHandle n;
-  ros::ServiceServer service = n.advertiseService("demo_service", demo_service_callback);
-  ros::spin();
-}
+    ROS_INFO("From Client [%s], Server says [%s]", req.in.c_str(), res.out.c_str());
 
-void thread_demo_clock_subscriber(int argc, char** argv)
-{
-  ros::init(argc, argv, "clock_subscriber");
-  ros::NodeHandle node_obj;
-  ros::Subscriber number_subscriber = node_obj.subscribe("/clock", 1, number_callback);
-  ros::spin();
-}
+    // double simTime = ros::Time::now().toSec();
+    // if (simTime > 20)
+    // {
+    //   ros::NodeHandle n;
+    //   ros::ServiceClient resetGazebo = n.serviceClient<std_srvs::Empty>("/gazebo/reset_world");
+    //   std_srvs::Empty srv;
+
+    //   if (resetGazebo.call(srv))
+    //   {
+    //     ROS_INFO("Resettig Gazebo World");
+    //   }
+    // }
+    return true;
+  }
+
+private:
+  int wallSequenceId;
+  int robotPositionId;
+  int bitmapId;
+  std::string mapType;
+};
 
 int main(int argc, char** argv)
 {
   // ros::Rate r(1);
+  ros::MultiThreadedSpinner spinner(2);
   ROS_INFO("Ready to receive from client.");
-  thread_demo_clock_subscriber(argc, argv);
-  thread_demo_service_server(argc, argv);
+  // thread_demo_clock_subscriber(argc, argv);
+  std::thread update(MessageHandler::thread_demo_clock_subscriber, argc, argv, spinner);
+  std::thread server(MessageHandler::thread_demo_service_server, argc, argv, spinner);
+
+  update.join();
+  server.join();
+
   return 0;
 }
